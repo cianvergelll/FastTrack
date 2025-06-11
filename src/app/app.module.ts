016608e5ca49eba52d77a0f6fcf993313adeccba
Ian <cianvergelll@gmail.com>
2025-06-11 14:10:28 +0800

    task manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { TaskManagerComponent } from './components/task-manager/task-manager.component';
+import { CalendarManagerComponent } from './components/calendar-manager/calendar-manager.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    AboutComponent,
+    TaskManagerComponent,
+    CalendarManagerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/calendar-manager/calendar-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-manager',
+  templateUrl: './calendar-manager.component.html',
+  styleUrls: ['./calendar-manager.component.css']
+})
+export class CalendarManagerComponent {
+
+}
