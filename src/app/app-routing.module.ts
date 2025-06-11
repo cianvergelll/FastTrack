7375ba13f8759bfe4fb57f15ed540a09d3ee2451
Ian <cianvergelll@gmail.com>
2025-06-11 15:24:12 +0800

    about page task manager page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { TaskManagerComponent } from './components/task-manager/task-manager.component';
+import { CalendarManagerComponent } from './components/calendar-manager/calendar-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutComponent, title: 'About Us' },
+  { path: 'task-manager', component: TaskManagerComponent, title: 'Task Manager' },
+  { path: 'calendar-manager', component: CalendarManagerComponent, title: 'Calendar Manager' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
